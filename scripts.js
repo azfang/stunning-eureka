5c09b94cc505472fc70e63a2a54453fcaf7213d1
azfang <47017488+azfang@users.noreply.github.com>
2025-11-16 16:29:13 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,7 @@
 var response = await fetch('https://api.are.na/v2/channels/illegibility/contents');
 var data = await response.json();
-// console.log(response);
-// console.log(data);
 
 var blocks = data.contents;
-// console.log(contents);
-// would log hi fourteen times, for each entry
-// contents.forEach(() => {
-//     console.log('hi');
-// })
 
 // can switch this out for a class
 var mainElement = document.querySelector('main');
